94cca162446290bb130f9046d732c5442ce7602c
rodney <rodneyjan.mandap@iwgplc.com>
2020-10-04 19:14:01 +0800

    Add new route to each sections

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
+
 app = Flask(__name__) 
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/team')
-def about():
+def team():
     return render_template('meet_the_team.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/environment')
+def environment():
+    return render_template('environment.html')
+
+@app.route('/insight')
+def insight():
+    return render_template('insights.html')
+
+@app.route('/methodology')
+def methodology():
+    return render_template('methodology.html')
+
+@app.route('/economics')
+def economic():
+    return render_template('socio_economics.html')
 
 
 if __name__ == '__main__':
